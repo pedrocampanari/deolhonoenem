4ea4fadfdee617313c250ea7451894173171084d
Pedro Campanari <pedrocampanari09@hotmail.com>
2024-08-08 13:03:25 -0300

    13h03

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log('Teste')
+const btnRedirect = document.getElementById('btn-video-library');
+
+btnRedirect.addEventListener("click", ()=>{
+    window.location.href = "./html/video-library.html";
+});
