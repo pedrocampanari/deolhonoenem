112a6455b263bfe83015e97fb1cd2a8998d38b08
Pedro Campanari <pedrocampanari09@hotmail.com>
2024-08-12 13:46:36 -0300

    13h46

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ btnRedirect.addEventListener("click", ()=>{
     window.location.href = "./html/video-library.html";
 });
 
+let newPhoto = ()=>{
+    let carousel = document.getElementById('carousel');
+    for(let i = 1; i <= 2; i++){
+        carousel.innerHTML += `
+        <div class="carousel-item">
+            <img src="assets/img/photoLibrary/foto${i}.jpeg" class="d-block w-100" alt="...">
+        </div>
+        `
+    }
+}
+
+newPhoto();
