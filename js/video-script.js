97515c62228ea974ae4ee4a5c4b4cb8a58d3f877
Pedro Campanari <pedrocampanari09@hotmail.com>
2024-08-14 14:48:29 -0300

    MentalMap

--- js/video-script.js
+++ js/video-script.js
@@ -5,6 +5,10 @@ const title = params.get('title');
 const VideoTitle = document.getElementById('title').innerText += " " + title;
 const VideoSource = document.getElementById('videoTag').setAttribute("src", `https://www.youtube.com/embed/${src}`);
 
+const mapAnchor = document.getElementById('mentalMap').innerText += ": '" + title + "'";
+const mapSource = document.getElementById('mentalMap').setAttribute("href", `../assets/mentalMap/${src}.pdf`);
+
+
 console.log(src, title);
 
 document.getElementById('btn-back').addEventListener("click", ()=>{
